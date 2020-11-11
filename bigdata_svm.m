04b06568d945997cd69d0306f1d943568eafe9f8
mental-disaster <54014203+mental-disaster@users.noreply.github.com>
2020-11-11 23:55:45 +0900

    Add files via upload

---
+++ bigdata_svm.m
@@ -0,0 +1,38 @@
+X = table2array(sam_s_data_s);
+Y = table2array(sam_s_label);
+
+disp("fitting svm")
+svm_Mdl = fitcecoc(X,Y);
+disp("fit complete")
+
+disp("fitting RF")
+RF_Mdl = fitcensemble(X,Y);
+disp("fit complete")
+
+
+ans = table2array(test_label);
+test_d = table2array(test_data);
+
+% pridict test data(SVM)
+output_svm = predict(svm_Mdl, test_d);
+cor = 0;
+for i = 1:length(output_svm)
+   if(output_svm(i) == ans(i))
+       cor = cor + 1;
+   end
+end
+
+svm_result = cor / length(output_svm)
+
+% pridict test data(RF)
+output_RF = predict(RF_Mdl, test_d);
+ans = table2array(test_label);
+cor = 0;
+
+for i = 1:length(output_RF)
+   if(output_RF(i) == ans(i))
+       cor = cor + 1;
+   end
+end
+
+RF_result = cor / length(output_RF)
